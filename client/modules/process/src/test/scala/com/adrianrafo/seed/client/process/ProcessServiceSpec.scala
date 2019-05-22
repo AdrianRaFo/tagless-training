7897f0742f5cf6cc9ae16d72ad0ec39a50db0fc8
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-05-22 15:58:42 +0200

    fix requested test

--- client/modules/process/src/test/scala/com/adrianrafo/seed/client/process/ProcessServiceSpec.scala
+++ client/modules/process/src/test/scala/com/adrianrafo/seed/client/process/ProcessServiceSpec.scala
@@ -4,9 +4,7 @@ import org.scalatest.FunSuite
 
 class ProcessServiceSpec extends FunSuite {
 
-  test("ProcessService should work with the http client"){}
-
-  test("ProcessService should work with the rpc client"){}
+  test("ProcessService should work with the client"){}
 
   test("ProcessService should fail if receives and empty name"){}
 
